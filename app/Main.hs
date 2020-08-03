9df87ace132256db323062ed628dc2dfaa5add55
kevin meyers <kevinm1776@gmail.com>
2020-08-03 02:19:55 -0400

    Json response.

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ExtendedDefaultRules #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TemplateHaskell #-}
@@ -9,24 +10,16 @@ import Lib
 
 import Yesod
 
-data Links = Links
+data App = App
 
-mkYesod "Links" [parseRoutes|
+mkYesod "App" [parseRoutes|
     / HomeR GET
-    /page1 Page1R GET
-    /page2 Page2R GET|]
+|]
 
-instance Yesod Links
+instance Yesod App
 
-getHomeR :: Handler Html
-getHomeR = defaultLayout 
-            [whamlet|<a href=@{Page1R}>Go to page 1!|]
-
-getPage1R = defaultLayout
-            [whamlet|<a href=@{Page2R}>Go to page 2!|]
-
-getPage2R = defaultLayout 
-            [whamlet|<a href=@{HomeR}>Go home!|]
+getHomeR :: HandlerFor App Value
+getHomeR = return $ object ["msg" .= "Hello, World"]
 
 main :: IO ()
-main = warp 3000 Links
+main = warp 3000 App
