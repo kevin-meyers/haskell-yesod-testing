3037c03e24b6ef21f8f2ab9217b748a007bef269
kevin meyers <kevinm1776@gmail.com>
2020-08-03 17:58:10 -0400

    Uses get and setMessages to hold info within a session.

--- app/Main.hs
+++ app/Main.hs
@@ -1,24 +1,9 @@
-{-# LANGUAGE ExtendedDefaultRules #-}
-{-# LANGUAGE QuasiQuotes #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE CPP #-}
-
-module Main where
-
-import Lib
-
-import Yesod
-import Text.Hamlet (hamlet, HtmlUrl)
-import Text.Blaze.Html.Renderer.String (renderHtml)
-import Data.Text (Text, append, pack)
-import Control.Arrow (second)
-import Network.HTTP.Types (renderQueryText)
-import Data.Text.Encoding (decodeUtf8)
-import Blaze.ByteString.Builder (toByteString)
-import Text.Lucius (CssUrl, luciusFile, luciusFileDebug, renderCss)
-import Data.Text.Lazy.IO as TLIO
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE QuasiQuotes           #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeFamilies          #-}
+import           Yesod
+import Data.Time (getCurrentTime)
 
 data App = App
 
@@ -26,35 +11,28 @@ mkYesod "App" [parseRoutes|
 / HomeR GET
 |]
 
-instance Yesod App
+instance Yesod App where
+    defaultLayout contents = do
+        PageContent title headTags bodyTags <- widgetToPageContent contents
+        mmsg <- getMessage
+        withUrlRenderer [hamlet|
+            $doctype 5
 
-getHomeR :: HandlerFor App Html
-getHomeR = defaultLayout $ do
-    setTitle "My Page Title"
-    toWidget [lucius| h1 { color: green; } |]
-    addScriptRemote "https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"
-    toWidget
-        [julius|
-            $(function() 
-            {
-                $("h1").click(function()
-                {
-                    alert("You clicked on the heading!");
-                });
-            });
-        |]
-    toWidgetHead
-        [hamlet|
-            <meta name=keywords content="some sample keywords">
-        |]
-    toWidget
-        [hamlet|
-            <h1>Here's one way of including content
-        |]
-    [whamlet|<h2>Here's another |]
-    toWidgetBody
-        [julius|
-            alert("This is included in the body itself");
+            <html>
+                <head>
+                    <title>#{title}
+                    ^{headTags}
+                <body>
+                    $maybe msg <- mmsg
+                        <div #message>#{msg}
+                    ^{bodyTags}
         |]
 
+getHomeR :: Handler Html
+getHomeR = do
+    now <- liftIO getCurrentTime
+    setMessage $ toHtml $ "You previously visited at: " ++ show now
+    defaultLayout [whamlet|<p>Try refreshing|]
+
+main :: IO ()
 main = warp 3000 App
