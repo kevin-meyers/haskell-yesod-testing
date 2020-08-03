29d894ff243541d57f127d92186e0719e019f10f
kevin meyers <kevinm1776@gmail.com>
2020-08-03 15:43:40 -0400

    Added several ways to use shakespeare templating.

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE CPP #-}
 
 module Main where
 
@@ -16,25 +17,44 @@ import Control.Arrow (second)
 import Network.HTTP.Types (renderQueryText)
 import Data.Text.Encoding (decodeUtf8)
 import Blaze.ByteString.Builder (toByteString)
+import Text.Lucius (CssUrl, luciusFile, luciusFileDebug, renderCss)
+import Data.Text.Lazy.IO as TLIO
 
+data App = App
 
-data MyRoute = Home | Time | Stylesheet
-
-render :: MyRoute -> [(Text, Text)] -> Text
-render Home _ = "/home"
-render Time _ = "/time"
-render Stylesheet _ = "/style.css"
-
-template :: Text -> HtmlUrl MyRoute
-template title = [hamlet|
-$doctype 5
-<html>
-    <head>
-        <title>#{title}
-        <link rel=stylesheet href=@{Stylesheet}>
-        <body>
-            <h1>#{title}
+mkYesod "App" [parseRoutes|
+/ HomeR GET
 |]
 
-main :: IO ()
-main = putStrLn $ renderHtml $ template "My Title" render
+instance Yesod App
+
+getHomeR :: HandlerFor App Html
+getHomeR = defaultLayout $ do
+    setTitle "My Page Title"
+    toWidget [lucius| h1 { color: green; } |]
+    addScriptRemote "https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"
+    toWidget
+        [julius|
+            $(function() 
+            {
+                $("h1").click(function()
+                {
+                    alert("You clicked on the heading!");
+                });
+            });
+        |]
+    toWidgetHead
+        [hamlet|
+            <meta name=keywords content="some sample keywords">
+        |]
+    toWidget
+        [hamlet|
+            <h1>Here's one way of including content
+        |]
+    [whamlet|<h2>Here's another |]
+    toWidgetBody
+        [julius|
+            alert("This is included in the body itself");
+        |]
+
+main = warp 3000 App
