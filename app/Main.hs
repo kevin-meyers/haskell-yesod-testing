ae6adb131ef7777385adb799f524c9071a7516f6
kevin meyers <kevinm1776@gmail.com>
2020-08-03 12:52:46 -0400

    First run with templating in hamlet.

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE ExtendedDefaultRules #-}
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
@@ -9,17 +8,25 @@ module Main where
 import Lib
 
 import Yesod
+import Text.Hamlet (shamlet)
+import Text.Blaze.Html.Renderer.String (renderHtml)
+import Data.Char (toLower)
+import Data.List (sort)
 
-data App = App
 
-mkYesod "App" [parseRoutes|
-    / HomeR GET
-|]
-
-instance Yesod App
-
-getHomeR :: HandlerFor App Value
-getHomeR = return $ object ["msg" .= "Hello, World"]
+data Person = Person
+    { name :: String
+    , age :: Int
+    }
 
 main :: IO ()
-main = warp 3000 App
+main = putStrLn $ renderHtml [shamlet|
+    <p>Hello, my name is #{name person} and I am #{show $ age person}.
+    <p>
+        Let's do some stuff with my name: #
+        <b>#{sort $ map toLower (name person)}
+    <p>Oh, and in 5 years I'll be #{show $(+) 5 $ age person} years old.
+    |]
+  where
+      person = Person "Kevin" 21
+
