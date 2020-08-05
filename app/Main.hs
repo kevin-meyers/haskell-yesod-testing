d0a4ff3726556f433347671c71727b94bea96661
kevin meyers <kevinm1776@gmail.com>
2020-08-05 14:47:11 -0400

    Created first form!

--- app/Main.hs
+++ app/Main.hs
@@ -1,42 +1,103 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE QuasiQuotes       #-}
-{-# LANGUAGE TemplateHaskell   #-}
-{-# LANGUAGE TypeFamilies      #-}
-import           Data.Text        (Text)
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE QuasiQuotes           #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeFamilies          #-}
+import           Control.Applicative ((<$>), (<*>))
+import           Data.Text           (Text)
+import           Data.Time           (Day)
 import           Yesod
+import           Yesod.Form.Jquery
 
 data App = App
 
 mkYesod "App" [parseRoutes|
-/      HomeR  GET
-/link1 Link1R GET
-/link2 Link2R GET
-/link3 Link3R GET
-/link4 Link4R GET
+/ HomeR GET
+/person PersonR POST
 |]
 
-instance Yesod App where
+instance Yesod App
 
+-- Tells our application to use the standard English messages.
+-- If you want i18n, then you can supply a translating function instead.
+instance RenderMessage App FormMessage where
+    renderMessage _ _ = defaultFormMessage
+
+-- And tell us where to find the jQuery libraries. We'll just use the defaults,
+-- which point to the Google CDN.
+instance YesodJquery App
+
+-- The datatype we wish to receive from the form
+data Person = Person
+    { personName          :: Text
+    , personBirthday      :: Day
+    , personFavoriteColor :: Maybe Text
+    , personEmail         :: Text
+    , personWebsite       :: Maybe Text
+    }
+  deriving Show
+
+-- Declare the form. The type signature is a bit intimidating, but here's the
+-- overview:
+--
+-- * The Html parameter is used for encoding some extra information. See the
+-- discussion regarding runFormGet and runFormPost below for further
+-- explanation.
+--
+-- * We have our Handler as the inner monad, which indicates which site this is
+-- running in.
+--
+-- * FormResult can be in three states: FormMissing (no data available),
+-- FormFailure (invalid data) and FormSuccess
+--
+-- * The Widget is the viewable form to place into the web page.
+--
+-- Note that the scaffolded site provides a convenient Form type synonym,
+-- so that our signature could be written as:
+--
+-- > personForm :: Form Person
+--
+-- For our purposes, it's good to see the long version.
+personForm :: Html -> MForm Handler (FormResult Person, Widget)
+personForm = renderDivs $ Person
+    <$> areq textField "Name" Nothing
+    <*> areq (jqueryDayField def
+        { jdsChangeYear = True -- give a year dropdown
+        , jdsYearRange = "1900:-5" -- 1900 till five years ago
+        }) "Birthday" Nothing
+    <*> aopt textField "Favorite color" Nothing
+    <*> areq emailField "Email address" Nothing
+    <*> aopt urlField "Website" Nothing
+
+-- The GET handler displays the form
 getHomeR :: Handler Html
-getHomeR = defaultLayout $ do
-    setTitle "Redirects"
-    [whamlet|
-        <p>
-            <a href=@{Link1R}>Click to start the redirect chain!
-    |]
+getHomeR = do
+    -- Generate the form to be displayed
+    (widget, enctype) <- generateFormPost personForm
+    defaultLayout
+        [whamlet|
+            <p>
+                The widget generated contains only the contents
+                of the form, not the form tag itself. So...
+            <form method=post action=@{PersonR} enctype=#{enctype}>
+                ^{widget}
+                <p>It also doesn't include the submit button.
+                <button>Submit
+        |]
 
-getLink1R, getLink2R, getLink3R :: Handler ()
-getLink1R = redirect Link2R -- /link2
-getLink2R = redirect (Link3R, [("foo", "bar")]) -- /link3?foo=bar
-getLink3R = redirect $ Link4R :#: ("baz" :: Text) -- /link4#baz
-
-getLink4R :: Handler Html
-getLink4R = defaultLayout
-    [whamlet|
-        <p>You made it!
-        <p>
-            <a href=@{HomeR}>Go back to home!
-    |]
+-- The POST handler processes the form. If it is successful, it displays the
+-- parsed person. Otherwise, it displays the form again with error messages.
+postPersonR :: Handler Html
+postPersonR = do
+    ((result, widget), enctype) <- runFormPost personForm
+    case result of
+        FormSuccess person -> defaultLayout [whamlet|<p>#{show person}|]
+        _ -> defaultLayout
+            [whamlet|
+                <p>
+                    <h1>Invalid input, let's try again!
+                    <a href=@{HomeR}>Go back to form
+            |]
 
 main :: IO ()
 main = warp 3000 App
