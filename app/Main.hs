e2b1f3f5f4db031e6c145486ab80878fcee1cf3b
kevin meyers <kevinm1776@gmail.com>
2020-08-06 03:06:31 -0400

    Added message set and get, also changed renderer.

--- app/Main.hs
+++ app/Main.hs
@@ -1,47 +1,51 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE QuasiQuotes           #-}
 {-# LANGUAGE TemplateHaskell       #-}
 {-# LANGUAGE TypeFamilies          #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-import           Control.Applicative ((<$>), (<*>))
-import qualified Web.ClientSession   as CS
 import           Yesod
 
 data App = App
 
 mkYesod "App" [parseRoutes|
-/ HomeR GET POST
+/            HomeR       GET
+/set-message SetMessageR POST
 |]
 
-getHomeR :: Handler Html
-getHomeR = do
-    sess <- getSession
-    defaultLayout
-        [whamlet|
-            <form method=post>
-                <input type=text name=key>
-                <input type=text name=val>
-                <input type=submit>
-            <h1>#{show sess}
-        |]
-
-postHomeR :: Handler ()
-postHomeR = do
-    (key, mval) <- runInputPost $ (,) <$> ireq textField "key" <*> iopt textField "val"
-    case mval of
-        Nothing -> deleteSession key
-        Just val -> setSession key val
-    liftIO $ print (key, mval)
-    redirect HomeR
-
 instance Yesod App where
-    -- Make the session timeout 1 minute so that it's easier to play with
-    makeSessionBackend _ = do
-        backend <- defaultClientSessionBackend 1 "keyfile.aes"
-        return $ Just backend
+    defaultLayout widget = do
+        pc <- widgetToPageContent widget
+        mmsg <- getMessage
+        withUrlRenderer
+            [hamlet|
+                $doctype 5
+                <html>
+                    <head>
+                        <title>#{pageTitle pc}
+                        ^{pageHead pc}
+                    <body>
+                        $maybe msg <- mmsg
+                            <p>Your message was: #{msg}
+                        ^{pageBody pc}
+            |]
 
 instance RenderMessage App FormMessage where
     renderMessage _ _ = defaultFormMessage
+
+getHomeR :: Handler Html
+getHomeR = defaultLayout
+    [whamlet|
+        <form method=post action=@{SetMessageR}>
+            My message is: #
+            <input type=text name=message>
+            <button>Go
+    |]
+
+postSetMessageR :: Handler ()
+postSetMessageR = do
+    msg <- runInputPost $ ireq textField "message"
+    setMessage $ toHtml msg
+    redirect HomeR
 
 main :: IO ()
 main = warp 3000 App
