78af95c43de80955d1d82ea298e7da09e0d4a0c9
kevin meyers <kevinm1776@gmail.com>
2020-08-06 01:43:19 -0400

    Added custom field type for password confirmation.

--- app/Main.hs
+++ app/Main.hs
@@ -3,101 +3,50 @@
 {-# LANGUAGE QuasiQuotes           #-}
 {-# LANGUAGE TemplateHaskell       #-}
 {-# LANGUAGE TypeFamilies          #-}
-import           Control.Applicative ((<$>), (<*>))
+import           Control.Applicative
 import           Data.Text           (Text)
-import           Data.Time           (Day)
 import           Yesod
-import           Yesod.Form.Jquery
 
 data App = App
 
 mkYesod "App" [parseRoutes|
 / HomeR GET
-/person PersonR POST
 |]
 
 instance Yesod App
 
--- Tells our application to use the standard English messages.
--- If you want i18n, then you can supply a translating function instead.
 instance RenderMessage App FormMessage where
     renderMessage _ _ = defaultFormMessage
 
--- And tell us where to find the jQuery libraries. We'll just use the defaults,
--- which point to the Google CDN.
-instance YesodJquery App
-
--- The datatype we wish to receive from the form
-data Person = Person
-    { personName          :: Text
-    , personBirthday      :: Day
-    , personFavoriteColor :: Maybe Text
-    , personEmail         :: Text
-    , personWebsite       :: Maybe Text
+passwordConfirmField :: Field Handler Text
+passwordConfirmField = Field
+    { fieldParse = \rawVals _fileVals ->
+        case rawVals of
+            [a, b]
+                | a == b -> return $ Right $ Just a
+                | otherwise -> return $ Left "Passwords don't match"
+            [] -> return $ Right Nothing
+            _ -> return $ Left "You must enter two values"
+    , fieldView = \idAttr nameAttr otherAttrs eResult isReq ->
+        [whamlet|
+            <input id=#{idAttr} name=#{nameAttr} *{otherAttrs} type=password>
+            <div>Confirm:
+            <input id=#{idAttr}-confirm name=#{nameAttr} *{otherAttrs} type=password>
+        |]
+    , fieldEnctype = UrlEncoded
     }
-  deriving Show
 
--- Declare the form. The type signature is a bit intimidating, but here's the
--- overview:
---
--- * The Html parameter is used for encoding some extra information. See the
--- discussion regarding runFormGet and runFormPost below for further
--- explanation.
---
--- * We have our Handler as the inner monad, which indicates which site this is
--- running in.
---
--- * FormResult can be in three states: FormMissing (no data available),
--- FormFailure (invalid data) and FormSuccess
---
--- * The Widget is the viewable form to place into the web page.
---
--- Note that the scaffolded site provides a convenient Form type synonym,
--- so that our signature could be written as:
---
--- > personForm :: Form Person
---
--- For our purposes, it's good to see the long version.
-personForm :: Html -> MForm Handler (FormResult Person, Widget)
-personForm = renderDivs $ Person
-    <$> areq textField "Name" Nothing
-    <*> areq (jqueryDayField def
-        { jdsChangeYear = True 
-        , jdsYearRange = "1900:-5"
-        }) "Birthday" Nothing
-    <*> aopt textField "Favorite color" Nothing
-    <*> areq emailField "Email address" Nothing
-    <*> aopt urlField "Website" Nothing
-
--- The GET handler displays the form
 getHomeR :: Handler Html
 getHomeR = do
-    -- Generate the form to be displayed
-    (widget, enctype) <- generateFormPost personForm
+    ((res, widget), enctype) <- runFormGet $ renderDivs
+        $ areq passwordConfirmField "Password" Nothing
     defaultLayout
         [whamlet|
-            <p>
-                The widget generated contains only the contents
-                of the form, not the form tag itself. So...
-            <form method=post action=@{PersonR} enctype=#{enctype}>
+            <p>Result: #{show res}
+            <form enctype=#{enctype}>
                 ^{widget}
-                <p>It also doesn't include the submit button.
-                <button>Submit
+                <input type=submit value="Change password">
         |]
-
--- The POST handler processes the form. If it is successful, it displays the
--- parsed person. Otherwise, it displays the form again with error messages.
-postPersonR :: Handler Html
-postPersonR = do
-    ((result, widget), enctype) <- runFormPost personForm
-    case result of
-        FormSuccess person -> defaultLayout [whamlet|<p>#{show person}|]
-        _ -> defaultLayout
-            [whamlet|
-                <p>
-                    <h1>Invalid input, let's try again!
-                    <a href=@{HomeR}>Go back to form
-            |]
 
 main :: IO ()
 main = warp 3000 App
