3e30b58dc596333d22e6becac4e718ccee7b2c68
kevin meyers <kevinm1776@gmail.com>
2020-08-06 03:04:34 -0400

    Added session key value pairs and timeout.

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,11 @@
-{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE QuasiQuotes           #-}
 {-# LANGUAGE TemplateHaskell       #-}
 {-# LANGUAGE TypeFamilies          #-}
-import           Control.Applicative
-import           Data.Text           (Text)
-import           Data.Time
+{-# LANGUAGE MultiParamTypeClasses #-}
+import           Control.Applicative ((<$>), (<*>))
+import qualified Web.ClientSession   as CS
 import           Yesod
-
--- In the authentication chapter, we'll address this properly
-newtype UserId = UserId Int
-    deriving Show
 
 data App = App
 
@@ -18,48 +13,35 @@ mkYesod "App" [parseRoutes|
 / HomeR GET POST
 |]
 
-instance Yesod App
+getHomeR :: Handler Html
+getHomeR = do
+    sess <- getSession
+    defaultLayout
+        [whamlet|
+            <form method=post>
+                <input type=text name=key>
+                <input type=text name=val>
+                <input type=submit>
+            <h1>#{show sess}
+        |]
+
+postHomeR :: Handler ()
+postHomeR = do
+    (key, mval) <- runInputPost $ (,) <$> ireq textField "key" <*> iopt textField "val"
+    case mval of
+        Nothing -> deleteSession key
+        Just val -> setSession key val
+    liftIO $ print (key, mval)
+    redirect HomeR
+
+instance Yesod App where
+    -- Make the session timeout 1 minute so that it's easier to play with
+    makeSessionBackend _ = do
+        backend <- defaultClientSessionBackend 1 "keyfile.aes"
+        return $ Just backend
 
 instance RenderMessage App FormMessage where
     renderMessage _ _ = defaultFormMessage
-
-type Form a = Html -> MForm Handler (FormResult a, Widget)
-
-data Blog = Blog
-    { blogTitle    :: Text
-    , blogContents :: Textarea
-    , blogUser     :: UserId
-    , blogPosted   :: UTCTime
-    }
-    deriving Show
-
-form :: UserId -> Form Blog
-form userId = renderDivs $ Blog
-    <$> areq textField "Title" Nothing
-    <*> areq textareaField "Contents" Nothing
-    <*> pure userId
-    <*> lift (liftIO getCurrentTime)
-
-getHomeR :: Handler Html
-getHomeR = do
-    let userId = UserId 5 -- again, see the authentication chapter
-    ((res, widget), enctype) <- runFormPost $ form userId
-    defaultLayout
-        [whamlet|
-            <form method=post action=@{HomeR} enctype=#{enctype}>
-                ^{widget}
-                <input type=submit>
-        |]
-
-postHomeR :: Handler Html
-postHomeR = do
-    let userId = UserId 5
-    ((res, widget), enctype) <- runFormPost $ form userId
-    defaultLayout 
-        [whamlet|
-            <p>Previous result: #{show res}
-            <a href=@{HomeR}>Go back
-        |]
 
 main :: IO ()
 main = warp 3000 App
